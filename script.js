974704aa0a5b0f204eb7776981b2a09409eb2780
BorgesLucas21 <141366250+BorgesLucas21@users.noreply.github.com>
2025-08-27 09:27:56 -0300

    2

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+
 console.log(botoes);
